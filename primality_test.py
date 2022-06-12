c8fb177d3da59c9546cd71094ecbc75c3b1dfb50
santitomatis <santiagotomatis06@gmail.com>
2022-06-12 20:50:51 -0300

    Just started with the test, basic structure defined

---
+++ primality_test.py
@@ -0,0 +1,15 @@
+def is_primal():
+    pass
+
+def run():
+    number = input("Type a number to execute a primality test: ")
+    if is_primal(number):
+        print("It is a prime number")
+    
+    else 
+    print("It is not a primer number")
+
+
+if __name__ == "__main__":
+    run()
+
