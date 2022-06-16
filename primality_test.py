49823544a22164b98c982bbd37b6d5bd19d0c3de
santitomatis <santiagotomatis06@gmail.com>
2022-06-15 23:26:24 -0300

    Started coding the primality test itself but still buggy

--- primality_test.py
+++ primality_test.py
@@ -1,13 +1,25 @@
-def is_primal():
-    pass
+def is_primal(number):
+    validator = True
+
+    for i in range(1, number + 1):
+        if i == 1 or i == number:
+            continue
+
+        if number % i == 0:
+            validator = False
+            return(validator)
+        
+        return(validator)
+
+        
 
 def run():
-    number = input("Type a number to execute a primality test: ")
+    number = int(input("Type a   number to execute a primality test: "))
     if is_primal(number):
         print("It is a prime number")
     
-    else 
-    print("It is not a primer number")
+    else: 
+        print("It is not a primer number")
 
 
 if __name__ == "__main__":
